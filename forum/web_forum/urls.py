86d1679e2546ff570941a9c8c94d262f838e4b26
Pandora <nimfanefor@gmail.com>
2023-08-12 16:32:10 +0600

    Добавил урлы для редакт-удал

--- forum/web_forum/urls.py
+++ forum/web_forum/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from web_forum.views.discus_view import DiscusListView, DiscusDetailView, DiscusUpdateView, DiscusCreateView, \
-    AnswerCreateView, UserProfileDetailView
+    AnswerCreateView, UserProfileDetailView, DiscusDeleteView, AnswerUpdateView, AnswerDeleteView
 
 app_name = "web_forum"
 
@@ -9,11 +9,9 @@ urlpatterns = [
     path('discus/<int:pk>', DiscusDetailView.as_view(), name="discus_detail_view"),
     path('discus/add/', DiscusCreateView.as_view(), name="discus_add"),
     path('discus/<int:pk>/update', DiscusUpdateView.as_view(), name="discus_update_view"),
-    # path('discus/<int:pk>/delete', ProjectDeleteView.as_view(), name="project_delete_view"),
+    path('discus/<int:pk>/delete', DiscusDeleteView.as_view(), name="discus_delete_view"),
     path('discus/<int:pk>/answers/add/', AnswerCreateView.as_view(), name="answer_add"),
     path('profile/', UserProfileDetailView.as_view(), name='user_profile'),
-    # path('answers/<int:pk>', TaskDetailView.as_view(), name="task_view"),
-    # path('answers/<int:pk>/update', TaskUpdateView.as_view(), name="task_update_view"),
-    # path('answers/<int:pk>/delete', TaskDeleteView.as_view(), name="task_delete_view"),
-
+    path('answers/<int:pk>/update', AnswerUpdateView.as_view(), name="answer_update_view"),
+    path('answers/<int:pk>/delete', AnswerDeleteView.as_view(), name="answer_delete_view"),
 ]
